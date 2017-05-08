149bb386aa2404a33c05c38a17bf60e96dfdbd24
Matheus Laureano <matheuslaureanol1@gmail.com>
2017-05-07 22:30:12 -0300

    Initial properties

---
+++ GerenciadorPassagens-war/src/java/controller/ClienteController.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import entities.Clientes;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import java.util.List;
+import javax.ejb.EJB;
+import model.ClientesFacade;
+
+/**
+ *
+ * @author Matheus
+ */
+@Named(value = "clienteController")
+@SessionScoped
+public class ClienteController implements Serializable {
+
+   
+    @EJB
+    private ClientesFacade clienteFacade;
+    private Clientes cliente = new Clientes();
+
+    public Clientes getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Clientes cliente) {
+        this.cliente = cliente;
+    }
+    public List<Clientes> findAll(){
+        return this.clienteFacade.findAll();
+    }
+    public ClienteController() {
+    }
+    
+    public String insert(){
+        this.clienteFacade.create(cliente);
+        this.cliente = new Clientes();
+        return "index";
+    }
+    
+    public void delete(Clientes cliente){
+        this.clienteFacade.remove(cliente);
+    }
+    
+    public String update(Clientes cliente){
+        this.cliente = cliente;
+        return "update";
+    }
+    
+    public String update(){
+        this.clienteFacade.edit(cliente);
+        this.cliente = new Clientes();
+        return "index";
+    }
+    
+}
